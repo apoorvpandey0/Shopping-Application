9cb822c6fe3ab9ecd0a0612cbc3ab466f591efcc
Apoorv Pandeym <apoorvpandey0@gmail.com>
2020-10-10 09:45:08 +0530

    Fixed autoLogout Duration

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -110,6 +110,6 @@ class Auth extends ChangeNotifier {
       _authTimer.cancel();
     }
     final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
-    _authTimer = Timer(Duration(seconds: 5), logout);
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
